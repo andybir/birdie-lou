313d582c78b031f5000f988a0448f2a2934ca97e
andybir <abirosak@gmail.com>
2020-04-27 15:55:03 -0400

    added favicon

--- shop/admin.py
+++ shop/admin.py
@@ -8,7 +8,7 @@ class CategoryAdmin(admin.ModelAdmin):
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['name', 'slug', 'price',
+    list_display = ['name', 'category', 'price',
                     'available', 'created', 'updated']
     list_filter = ['available', 'created', 'updated']
     list_editable = ['price', 'available']
