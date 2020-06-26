427cad9ee153c26b930070740e755bbf29b4441e
andybir <abirosak@gmail.com>
2020-06-26 14:18:42 -0400

    modified cc accepted/cc declined pages

--- cart/forms.py
+++ cart/forms.py
@@ -1,5 +1,4 @@
 from django import forms
-from django.utils.translation import gettext_lazy as _
 
 PRODUCT_QUANTITY_CHOICES = [(i, str(i)) for i in range(1, 21)]
 
@@ -7,7 +6,7 @@ class CartAddProductForm(forms.Form):
     quantity = forms.TypedChoiceField(
                             choices=PRODUCT_QUANTITY_CHOICES,
                             coerce=int,
-                            label=_('quantity'))
+                            label=('quantity'))
     update = forms.BooleanField(required=False,
                                 initial=False,
                                 widget=forms.HiddenInput)
