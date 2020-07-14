534c76b37a91f417f733638feec83e6481378c94
andybir <abirosak@gmail.com>
2020-07-14 14:59:39 -0400

    updated settings->DEBUG. updated .gitignore

--- birdie_lou/settings.py
+++ birdie_lou/settings.py
@@ -20,10 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '44=w$0r2s@_14$9#mat(vgc)24c0b@uojf3j%hn4nk!pp^p&=*'
+# SECRET_KEY = '44=w$0r2s@_14$9#mat(vgc)24c0b@uojf3j%hn4nk!pp^p&=*'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '44=w$0r2s@_14$9#mat(vgc)24c0b@uojf3j%hn4nk!pp^p&=*')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = []
 
